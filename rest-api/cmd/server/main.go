0d76159fbb4ba1fa69108fba3b182eb0523e283e
Benjamin Buetikofer <me@bueti-online.ch>
2022-04-23 20:59:22 +0200

    database connection

--- rest-api/cmd/server/main.go
+++ rest-api/cmd/server/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/bueti/learn_go/rest-api/internal/db"
+)
 
 // is responsible for the instantiation
 // and startup of our application
 func Run() error {
 	fmt.Println("starting up our application")
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err == nil {
+		return err
+	}
+
 	return nil
 }
 
--- rest-api/go.mod
+++ rest-api/go.mod
@@ -1,3 +1,8 @@
 module github.com/bueti/learn_go/rest-api
 
 go 1.17
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+)
